eb0747dc95cc0c1082b9c037dedf29851aec8fba
gfsxyz <gustifaizals@gmail.com>
2023-06-21 13:42:49 +0800

    song upload modal

--- components/Library.tsx
+++ components/Library.tsx
@@ -2,10 +2,19 @@
 
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
 
 const Library = () => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
   const onClick = () => {
-    //handle upload onclick
+    //open login modal if user does not exist/not auth
+    if (!user) return authModal.onOpen();
+
+    return uploadModal.onOpen();
   };
 
   return (
@@ -13,15 +22,15 @@ const Library = () => {
       <div className="flex items-center justify-between px-5 pt-4">
         <div className="inline-flex items-center gap-x-2">
           <TbPlaylist className="text-neutral-400" size={26} />
-          <p className="text-neutral-400 font-medium text-md">Your Library</p>
+          <p className="font-medium text-neutral-400 text-md">Your Library</p>
         </div>
         <AiOutlinePlus
           onClick={onClick}
           size={20}
-          className="text-neutral-400 cursor-pointer hover:text-white transition"
+          className="transition cursor-pointer text-neutral-400 hover:text-white"
         />
       </div>
-      <div className="flex-flex-col gap-y-2 mt-4 px-3">List of Songs!</div>
+      <div className="px-3 mt-4 flex-flex-col gap-y-2">List of Songs!</div>
     </div>
   );
 };
